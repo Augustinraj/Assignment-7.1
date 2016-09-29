8872e143e1a6fbfcdabedb6266c7255c1f5cd38a
Augustin <Augustinraj@users.noreply.github.com>
2016-09-29 22:49:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+//Function name calculate area//
+function calculateArea() {
+    //Declaring radius as 7//
+var Radius = 7;
+    //formula to calculate the area of circle//
+var result = (Radius * Radius* Math.PI );
+    //displaying in console//
+console.log(result);
+}
